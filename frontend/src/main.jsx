159e362f90483b30565a9996c49f1bfff5a564be
siddheshThorat04 <siddheshthorat2004@gmail.com>
2025-01-19 22:50:23 +0530

    minor issuer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChatContextProvider } from './contextApi/ChatContext';
+import { ChatContextProvider } from './contextApi/ChatContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './contextApi/authContext.jsx';
 import { DarkThemeContextProvider } from './contextApi/DarkTheme.jsx';
